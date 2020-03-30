80fed97f2c441ec276be939df4edf3768d922280
rustamsariyev <r.sariyev@outlook.com>
2020-03-30 09:55:37 +0000

    while-task01.sh file modified

--- while-task01.sh
+++ while-task01.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 #
+# This script changes and increment strings
+# You will be promted to enter XML_URL and save location
 
+# Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
+then
+	echo 'Please run with sudo or root.'
+	exit 1
+fi
+
+# Get the XML file
 read -p 'Enter the file path:' XML_URL
+
+# Get the XML save location
 read -p 'Enter the path to save:' XML_SAVE
 
+# Create new directory for new files
 
 cd $XML_SAVE
 mkdir Copied
 
+# Set variable
 i=0
 
+# Create while loop
 while [[ "$i" -lt 10 ]] 
 do
 	((i++))
@@ -18,5 +33,16 @@ do
 	mv Text* "$XML_SAVE/Copied"
 done
 
-echo $XML_URL
-echo $XML_SAVE
+# If command not successfully executed change exit status 1 
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The file not changed.'
+	exit 1
+fi
+
+# Display the XML_URL and XML_SAVE location where the file was saved.  
+echo 'XML_URL'
+echo "${XML_URL}"
+echo 'XML_SAVE'
+echo "${XML_SAVE}"
+exit 0
